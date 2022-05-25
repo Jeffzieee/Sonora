e8a0a46a66d144fa3cc20fefe538aef62da91d4c
Jephin John <jephinjohn85@gmail.com>
2022-05-25 11:16:45 +0530

    25/05/2022 - Wednesday ->added curve_Shape.xml ->Background Color modified by adding changes to themes.xml

--- app/src/main/java/com/codered/sonora/MainActivity.kt
+++ app/src/main/java/com/codered/sonora/MainActivity.kt
@@ -3,7 +3,7 @@ package com.codered.sonora
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() { //hello
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
