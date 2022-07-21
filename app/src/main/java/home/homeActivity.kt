7a18453bc4cdc58ed0ac862cd1db1ec91555ecac
Jephin John <jephinjohn85@gmail.com>
2022-07-21 20:01:10 +0530

    21/07/2022 - Thursday ->Final Commit before Second Review

--- app/src/main/java/home/homeActivity.kt
+++ app/src/main/java/home/homeActivity.kt
@@ -1,23 +1,43 @@
-/* SONORA - RETRY Activity
-   Language - Kotlin
-   Code - jeffzieee
-   Team - CodeRED. 2022
- */
-
 package home
 
-//importing Dependancies
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.codered.sonora.databinding.ActivityHomeBinding
-
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import com.codered.sonora.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class homeActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityHomeBinding
+
+    private val homeFragment = home.homeFragment()
+    private val libraryFragment = home.libraryFragment()
+    private val searchFragment = home.searchFragment()
+    private val upgradeFragment = home.upgradeFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val navDock = findViewById<BottomNavigationView>(R.id.naviDock)
 
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        replaceFragment(homeFragment)
+
+        navDock.setOnItemSelectedListener {
+            when(it.itemId){
+                  R.id.home->replaceFragment(homeFragment)
+                  R.id.search->replaceFragment(searchFragment)
+                  R.id.library->replaceFragment(libraryFragment)
+                  R.id.upgrade->replaceFragment(upgradeFragment)
+            }
+            true
+        }
+
+
+    }
+    private fun replaceFragment(fragment : Fragment){
+        if(fragment != null){
+           val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_Container, fragment)
+            transaction.commit()
+        }
     }
 }
